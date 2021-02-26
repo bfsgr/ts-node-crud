6f4c9f0f8c712643289730c46203cfd0c450bedb
bfsgr <bfsgr.code@protonmail.com>
2021-02-26 14:08:55 -0300

    prettier

--- src/entity/student.entity.ts
+++ src/entity/student.entity.ts
@@ -1,21 +1,20 @@
-import {Entity, BaseEntity, Column, PrimaryGeneratedColumn} from "typeorm";
+import { Entity, BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Student extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+	@Column({
+		length: 50,
+	})
+	first_name: string;
 
-    @Column({
-        length: 50
-    })
-    first_name: string;
-    
-    @Column({
-        length: 255
-    })
-    last_name: string;
+	@Column({
+		length: 255,
+	})
+	last_name: string;
 
-    @Column()
-    year_of_admission: number;
+	@Column()
+	year_of_admission: number;
 }
--- src/migration/1614315310461-CreateStudent.ts
+++ src/migration/1614315310461-CreateStudent.ts
@@ -1,36 +1,36 @@
-import {MigrationInterface, Table, QueryRunner} from "typeorm";
+import { MigrationInterface, Table, QueryRunner } from 'typeorm';
 
 export class CreateStudent1614313987385 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'student',
+				columns: [
+					{
+						name: 'id',
+						type: 'integer',
+						isPrimary: true,
+						isGenerated: true,
+						generationStrategy: 'increment',
+					},
+					{
+						name: 'first_name',
+						type: 'varchar(50)',
+					},
+					{
+						name: 'last_name',
+						type: 'varchar(255)',
+					},
+					{
+						name: 'year_of_admission',
+						type: 'integer',
+					},
+				],
+			})
+		);
+	}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table({
-            name: "student",
-            columns: [
-                {
-                    name: "id",
-                    type: "integer",
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: "increment"
-                },
-                {
-                    name: "first_name",
-                    type: "varchar(50)",
-                },
-                {
-                    name: "last_name",
-                    type: "varchar(255)",
-                },
-                {
-                    name: "year_of_admission",
-                    type: "integer",
-                }
-            ]
-        }))
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("student");
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('student');
+	}
 }
