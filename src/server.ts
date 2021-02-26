d7006c885cd298bcd73e53a301001b2fcc5257a0
bfsgr <bfsgr.code@protonmail.com>
2021-02-26 14:12:46 -0300

    static handler

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ import * as bodyParser from 'body-parser';
 
 import StudentController from './controllers/student.controller';
 
-import EventHandler from './utils/event.handler';
+import eventHandler from './utils/event.handler';
 
 const app = new App({
 	host: 'localhost',
 	port: 5000,
 	controllers: [new StudentController()],
 	middleWares: [bodyParser.json(), bodyParser.urlencoded({ extended: true })],
-	handlers: new EventHandler().init(),
+	handlers: eventHandler.init(),
 });
 
 app.listen();
